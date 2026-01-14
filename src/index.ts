87760ee4b9af43043be03dc00abc7f473fdf49ae
kennywam <kennywambu254@gmail.com>
2026-01-14 14:04:56 +0300

    feat: Enhance REPL functionality to parse and execute SQL statements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-import { startRepl } from "./repl/repl";
+import { startRepl } from './repl/repl'
+import { parse } from './parser/parser'
+import { Executor } from './executor/executor'
+import { Database } from './catalog/database'
 
-startRepl((input: string) => {
-  console.log("You typed:", input);
-});
+const dbManager = new Database()
+const executor = new Executor(dbManager)
+
+startRepl((input) => {
+  try {
+    const statement = parse(input)
+    if (!statement) {
+      console.log('Invalid or unsupported SQL')
+      return
+    }
+
+    const result = executor.execute(statement)
+    console.log(result)
+  } catch (err: any) {
+    console.error('Error:', err.message)
+  }
+})
